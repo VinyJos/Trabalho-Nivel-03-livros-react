7feb44cd23f7927923556999bc74e12e11fc3c5b
Vinicius Jose <viniciuswk@gmail.com>
2023-03-03 21:32:59 -0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import LivroLista from './LivroLista';
+import LivroDados from './LivroDados';
 import ControleLivro from './controle/ControleLivros';
 
 function App() {
   const controleLivros = new ControleLivro();
 
   return (
-    <div className="container mt-4">
-      <LivroLista livros ={controleLivros.obterLivros()} />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Catalago</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/dados">Novo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LivroLista livros={controleLivros.obterLivros()} />} />
+          <Route path="/dados" element={<LivroDados />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/LivroDados.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LivroDados = () => {
+  return (
+    <main>
+      <p>Olá mundo</p>
+    </main>
+  );
+};
+
+export default LivroDados;
